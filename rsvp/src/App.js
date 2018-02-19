fd5404be97a1d74005aa9ec11d96442120964558
Chris Underwood <cunderwoodmn@gmail.com>
2018-02-18 19:55:42 -0800

    add functionality to read our user object and display them as react stateful objects on the screen

--- rsvp/src/App.js
+++ rsvp/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './App.css';
+import GuestList from './GuestList';
 
 class App extends Component {
 
@@ -10,6 +12,10 @@ class App extends Component {
       },
       {
         name: 'Lily',
+        isConfirmed: true
+      },
+      {
+        name: 'Melanie',
         isConfirmed: true
       }
     ]
@@ -53,32 +59,7 @@ class App extends Component {
               </tr>
             </tbody>
           </table>
-          <ul>
-            <li className="pending"><span>Safia</span></li>
-            <li className="responded"><span>Iver</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li className="responded">
-              <span>Corrina</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li>
-              <span>Joel</span>
-              <label>
-                <input type="checkbox" /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-          </ul>
+          <GuestList guests={this.state.guests}/>
         </div>
       </div>
     );
